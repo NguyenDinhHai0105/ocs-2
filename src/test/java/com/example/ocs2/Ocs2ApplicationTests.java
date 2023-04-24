9eded86046530dad2ce29b24274c32f8eb6737bd
Hải nguyễn đình <haiholmes99@gmail.com>
2023-04-25 01:20:59 +0700

    first commit

---
+++ src/test/java/com/example/ocs2/Ocs2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ocs2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ocs2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
